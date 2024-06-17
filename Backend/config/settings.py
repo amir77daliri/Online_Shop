064835ac59b019d4d697572d26fdd86b95336a53
Amirhossein_Daliri <amirdalir0111@gmail.com>
2024-06-17 15:09:16 +0330

    config swagger authorize with header key

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -87,6 +87,16 @@ REST_FRAMEWORK = {
     ]
 }
 
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    },
+}
+
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
